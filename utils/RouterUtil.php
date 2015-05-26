f37b015cf4c4b8e4f6c7a0d42c83f62d23b12e90
flareair <flareair0@gmail.com>
2015-05-26 10:22:24 +0300

    Comments in RouterUtil changed to match PHPdoc conventions

--- utils/RouterUtil.php
+++ utils/RouterUtil.php
@@ -2,23 +2,21 @@
 
 namespace utils;
 
-
-/*
-
-  Bacis Router class
-
-*/
+/**
+ * Basis Router class
+ */
 
 class RouterUtil {
 
-  /*
-    This method selects propertly controller and its action, or render 404 error page
-    @param string $requestMethod - request method (GET, POST, etc)
-    @param string $requestUri - contains request URI
-    $param array $getArray - contains $_GET array
-    $param array $postArray - contains $_POST array
-    @returns null
-  */
+  /**
+    * This method selects propertly controller and its action, or render 404
+    * error page
+    * @param string $requestMethod - request method (GET, POST, etc)
+    * @param string $requestUri - contains request URI
+    * @param array $getArray - contains $_GET array
+    * @param array $postArray - contains $_POST array
+    * @return null
+    */
   public function route($requestMethod, $requestUri, $getArray, $postArray) {
     $parsedUri = $this->parseUri($requestUri);
     $controllerName = $this->getControllerName($parsedUri);
@@ -27,23 +25,21 @@ class RouterUtil {
       require($_SERVER['DOCUMENT_ROOT'] . '/views/errors/404.php');
     }
   }
-  /*
-    This method explodes URI based on '/' and removes query string
-    @param string $uri - URI string
-    @returns array
-  */
+  /**
+    * This method explodes URI based on '/' and removes query string
+    * @param string $uri - URI string
+    * @return array
+    */
   protected function parseUri($uri) {
     $uriArray = explode('/', strtok($uri, '?'));
     return $uriArray;
   }
 
-  /*
-
-    This method produces controller name based on URI
-    @param array $parsedUri
-    @returns string
-
-  */
+  /**
+   * This method produces controller name based on URI
+   * @param array $parsedUri
+   * @return string
+   */
 
   protected function getControllerName($parsedUri) {
     if ($parsedUri[1] === '') {
@@ -51,6 +47,12 @@ class RouterUtil {
     }
     return 'controllers\\'. ucfirst($parsedUri[1]) . 'Controller';
   }
+
+  /**
+   * Method returnts controller action (method name) based on URI
+   * @param array $parseUri - parsed URI array
+   * @return string
+   */
 
   protected function getActionName($parsedUri) {
     // Added to handle root (index) action
@@ -63,15 +65,13 @@ class RouterUtil {
     return 'action' . ucfirst($parsedUri[2]);
   }
 
-
-  /*
-    This method launches class method (action). Returns false if class or method doesn't exist
-    @param string $controllerName - name of controller
-    @param string @actionName - name of action
-    @returns bool
-
-  */
-
+  /**
+   * This method launches class method (action). Returns false if class or
+   * method doesn't exist
+   * @param string $controllerName - name of controller
+   * @param string @actionName - name of action
+   * @return bool
+   */
 
   protected function fireClass($controllerName, $actionName) {
     // Check if controller and its method exists
